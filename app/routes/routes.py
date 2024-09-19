09fd916c7986567e368bb7a71194fcfb829d900b
ujin6666 <ujin736@gmail.com>
2024-09-19 22:53:36 +0900

    회원 정보 라우트 블루프린트 등록

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 from app.routes.test_api import test_api_bp
 from app.routes.react import react_bp
 from app.routes.test_db import test_db_bp
+from app.routes.user_information_route import user_information_bp
 
 def init_routes(app):
     app.register_blueprint(test_api_bp)
     app.register_blueprint(react_bp)
     app.register_blueprint(test_db_bp)
+    app.register_blueprint(user_information_bp)
