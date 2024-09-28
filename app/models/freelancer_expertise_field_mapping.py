840a3be0a1c427147d05273b9f47897d225a7fdd
ujin6666 <ujin736@gmail.com>
2024-09-28 19:06:22 +0900

    PK 추가

--- app/models/freelancer_expertise_field_mapping.py
+++ app/models/freelancer_expertise_field_mapping.py
@@ -6,7 +6,7 @@ class FreelancerExpertiseFieldMapping(db.Model):
     __tablename__ = 'FREELANCER_EXPERTISE_FIELD_MAPPING'
     
     public_profile_id = db.Column(db.String(50), db.ForeignKey('PUBLIC_PROFILE.public_profile_id', ondelete='CASCADE'), primary_key=True)
-    field_code = db.Column(db.String(20), db.ForeignKey('FIELD_KEYWORDS.field_code', ondelete='CASCADE'))
+    field_code = db.Column(db.String(20), db.ForeignKey('FIELD_KEYWORDS.field_code', ondelete='CASCADE'), primary_key=True)
     
     public_profile = db.relationship('PublicProfile', backref=db.backref('expertise_fields', lazy=True, cascade="all, delete-orphan"))
     field_keywords = db.relationship('FieldKeywords', backref=db.backref('freelancer_expertise_mappings', lazy=True, cascade="all, delete-orphan"))
