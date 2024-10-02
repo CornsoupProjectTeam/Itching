974e1091fd4326058e625492b75a68436a703806
ujin6666 <ujin736@gmail.com>
2024-10-02 21:55:50 +0900

    연동을 위한 모델 수정

--- app/models/chat_room_scanner_requirement.py
+++ app/models/chat_room_scanner_requirement.py
@@ -1,7 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
 from datetime import datetime
-
-db = SQLAlchemy()
 
 class ChatRoomScannerRequirement(db.Model):
     __tablename__ = 'CHAT_ROOM_SCANNER_REQUIREMENT'
--- app/models/recommendation.py
+++ app/models/recommendation.py
@@ -1,7 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
 from datetime import datetime
-
-db = SQLAlchemy()
 
 class Recommendation(db.Model):
     __tablename__ = 'RECOMMENDATION'
