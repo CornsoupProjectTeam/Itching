ce3dbec32c93843dd2b962d12ed4ca1fb4485580
ujin6666 <ujin736@gmail.com>
2024-10-04 09:08:34 +0900

    프리랜서 등록 라우트 등록

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -6,6 +6,7 @@ from app.routes.user_information_route import user_information_bp
 from app.routes.login_route import login_bp
 from app.routes.payment_authentication_route import payment_authentication_bp
 from app.routes.identitiy_verification_route import identity_verification_bp
+from app.routes.freelancer_registration_route import freelancer_registration_bp
 
 def init_routes(app):
     app.register_blueprint(test_api_bp)
@@ -15,3 +16,4 @@ def init_routes(app):
     app.register_blueprint(login_bp)
     app.register_blueprint(payment_authentication_bp)
     app.register_blueprint(identity_verification_bp)
+    app.register_blueprint(freelancer_registration_bp)
