b3bc3b996a9b391ab75b3cc8be3af6449c6b829e
ujin6666 <ujin736@gmail.com>
2024-10-08 03:03:08 +0900

    도연 코드 돌리기 위해 삭제

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
 from flask_socketio import SocketIO
 from flask_mail import Mail
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -29,9 +28,6 @@ db = SQLAlchemy(app)
 mongo = PyMongo(app)
 mail = Mail(app)
 
-# Flask-Migrate 초기화
-migrate = Migrate(app, db)
-
 # 보안 설정
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
@@ -40,6 +36,7 @@ app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 from app.routes.routes import init_routes
 init_routes(app)
 
+#app/routes/routes.py에서 등록할 것
 from app.routes.chat_room_routes import chat_room_bp
 app.register_blueprint(chat_room_bp, url_prefix='/chatroom')
 
