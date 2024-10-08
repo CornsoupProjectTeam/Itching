8a6951093a9b7d164afc661b48f2a84ac395c371
ujin6666 <ujin736@gmail.com>
2024-10-08 17:20:53 +0900

    클라이언트 글 모델 합병

--- app/models/client_post_reference_image_mapping.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class ClientPostReferenceImageMapping(db.Model):
-    __tablename__ = 'CLIENT_POST_REFERENCE_IMAGE_MAPPING'
-    
-    client_post_id = db.Column(db.String(50), db.ForeignKey('CLIENT_POST.client_post_id', ondelete='CASCADE'), primary_key=True)
-    reference_image_path = db.Column(db.String(255))
-    
-    client_post = db.relationship('ClientPost', backref=db.backref('reference_images', uselist=False, cascade="all, delete-orphan"))
